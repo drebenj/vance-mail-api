0da36b432d2ab6b39ef01390557f3a24df833e84
JoshDreben <JoshDreben@users.noreply.github.com>
2020-07-27 15:08:34 -0700

    :fire: fix post request

--- api/members/subscribe.js
+++ api/members/subscribe.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.MAILCHIMP_API_KEY;
 const Mailchimp = require("mailchimp-api-v3");
 const mailchimp = new Mailchimp(API_KEY);
 
-function subscribe(req, res) {
+module.exports = async (req, res) => {
   const data = {
     email_address: req.body.email,
     status: "subscribed",
@@ -17,6 +17,4 @@ function subscribe(req, res) {
     .catch((err) => {
       res.status(err.status ? err.status : 400).json(err);
     });
-}
-
-module.exports = subscribe;
+};
